94237df1859c5ed5f5a6710dabc334562c827c13
ToeCrow <thomas.kronvold@chasacademy.se>
2025-01-20 13:13:23 +0100

    index och data måste ligga i root

---
+++ src/Pages/TripList/TripList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ActivityList from '../ActivityList/ActivityList'
+
+const TripList = () => {
+  return (
+    <main>
+      <h2>Dina planerade resor</h2>
+      <ActivityList />
+    </main>
+  )
+}
+
+export default TripList
