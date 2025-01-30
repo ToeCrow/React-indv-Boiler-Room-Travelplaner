d229991923fa4a8e3617955f1f23b52c60115e11
ToeCrow <thomas.kronvold@chasacademy.se>
2025-01-30 18:53:24 +0100

    Converted to a RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import Header from './Components/Header/Header';
-import './index.css';
-import ActivityForm from './Pages/ActivityForm/ActivityForm';
-import ActivityList from './Pages/ActivityList/ActivityList';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import EditActivity from './Pages/EditActivity/EditActivity ';
-import NotFound from './Components/NotFound/NotFound';
-import About from './Pages/About/About';
-import ExoticTravels from './Pages/ExoticTravels/ExoticTravels';
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header/Header";
 
-function App() {
+const App= () => {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<ActivityList />} />
-        <Route path="/add-activity" element={<ActivityForm />} />
-        <Route path="/edit-activity/:id" element={<EditActivity />} />
-        <Route path="/exotictravels" element={<ExoticTravels />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <Outlet /> {/* Här laddas de olika sidorna */}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,28 @@
+import React from "react"; // Se till att importera React
+import { createBrowserRouter } from "react-router-dom"; // Importera router-metod från react-router-dom
+import App from "./App";
+import ActivityList from "./Pages/ActivityList/ActivityList";
+import ActivityForm from "./Pages/ActivityForm/ActivityForm";
+import EditActivity from "./Pages/EditActivity/EditActivity";
+import About from "./Pages/About/About";
+import ExoticTravels from "./Pages/ExoticTravels/ExoticTravels";
+import NotFound from "./Components/NotFound/NotFound";
+
+// Skapa router med createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Välj AppLayout för huvudkomponenten
+    children: [
+      { index: true, element: <ActivityList /> }, // Default route
+      { path: "add-activity", element: <ActivityForm /> },
+      { path: "edit-activity/:id", element: <EditActivity /> },
+      { path: "exotictravels", element: <ExoticTravels /> },
+      { path: "about", element: <About /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+// Exportera router-konfigurationen
+export default router;
